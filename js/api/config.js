712c54c7dd18ec5948b1afee4cd9a3b0c3b4c27e
elmohawk <mrhawk2007@gmail.com>
2026-07-11 13:40:22 +0200

    Create config.js

---
+++ js/api/config.js
@@ -0,0 +1,26 @@
+// TMDB SETTINGS
+
+TMDB_API_KEY:
+
+"8b8937bf3e114fa3502358a4f090c0df",
+
+
+TMDB_BASE_URL:
+
+"https://api.themoviedb.org/3",
+
+
+TMDB_IMAGE:
+
+{
+
+    poster:
+    "https://image.tmdb.org/t/p/w500",
+
+    posterLarge:
+    "https://image.tmdb.org/t/p/w780",
+
+    backdrop:
+    "https://image.tmdb.org/t/p/original"
+
+},
