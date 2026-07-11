bd67380b77472b6f1d28047f2c75174f6e23476b
elmohawk <mrhawk2007@gmail.com>
2026-07-11 14:58:29 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,25 +3,18 @@
    CONFIGURATION
 =========================================== */
 
-
 export const CONFIG = {
-
-
     // Application
 
     APP_NAME: "KivuStream",
 
     VERSION: "ALT3",
 
-
-
-    // Cloudflare Worker
+    // Cloudflare Worker (future)
 
     WORKER_URL:
 
     "YOUR_CLOUDFLARE_WORKER_URL",
-
-
 
     // Supabase
 
@@ -29,38 +22,40 @@ export const CONFIG = {
 
     "https://exjgejujfxejjlbfizgz.supabase.co",
 
-
     SUPABASE_ANON_KEY:
 
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4amdlanVqZnhlampsYmZpemd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MTQzMTQsImV4cCI6MjA5NDA5MDMxNH0.CWUYLk4qJfriIYXWScB7wcHHVTCuz0SGDhWUV3tMR1Y",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4amdlanVqZnhlampsYmZpemd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MTQzMTQsImV4cCI6MjA5NDA5MDMxNH0.CWUYLk4qJfriIYXWScB7wcHHVTCuz0SGDhWUV3tMR1Y";
+
+    // ===========================
+    // TMDB SETTINGS
+    // ===========================
 
 
+    TMDB_API_KEY:
 
+    "8b8937bf3e114fa3502358a4f090c0df",
 
-    // TMDB IMAGE SETTINGS
+    TMDB_BASE_URL:
+
+    "https://api.themoviedb.org/3",
+
 
     TMDB_IMAGE:
 
     {
-
         poster:
 
         "https://image.tmdb.org/t/p/w500",
-
 
         posterLarge:
 
         "https://image.tmdb.org/t/p/w780",
 
-
         backdrop:
 
-        "https://image.tmdb.org/t/p/original"
+        "https://image.tmdb.org/t/p/original",
 
     },
-
-
-
 
     // Default Settings
 
@@ -69,47 +64,21 @@ export const CONFIG = {
 
     "en-US",
 
-
     DEFAULT_REGION:
 
     "RW",
-
-
-
-    // Pagination
-
 
     ITEMS_PER_PAGE:
 
     20,
 
-
-
-    // Features
-
-
     FEATURES:
 
     {
-
-
         AUTH:true,
-
-
         COMMENTS:true,
-
-
         FAVORITES:true,
-
-
         WATCH_HISTORY:true,
-
-
-        DOWNLOADS:true
-
-
+        DOWNLOADS:true,
     }
-
-
-
 };
