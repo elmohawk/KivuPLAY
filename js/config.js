505b1a6c34bec617d389d59e1238bf274409ec7d
elmohawk <mrhawk2007@gmail.com>
2026-07-11 13:01:08 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+export const CONFIG = {
+
+    APP_NAME: "KivuStream",
+
+    WORKER_URL: "https://YOUR-WORKER.workers.dev",
+
+    SUPABASE_URL: "",
+
+    SUPABASE_KEY: "",
+
+    IMAGE_BASE: "https://image.tmdb.org/t/p/w780",
+
+    BACKDROP_BASE: "https://image.tmdb.org/t/p/original",
+
+    DEFAULT_LANGUAGE: "en-US"
+
+};
